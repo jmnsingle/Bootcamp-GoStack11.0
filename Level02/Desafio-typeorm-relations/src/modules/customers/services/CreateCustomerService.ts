99095647ad6045da17505318800722deff895b26
Juliano Nogueira <judev.software@gmail.com>
2020-10-20 17:02:43 -0300

    Challenge-relations added.

---
+++ Level02/Desafio-typeorm-relations/src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Customer from '../infra/typeorm/entities/Customer';
+import ICustomersRepository from '../repositories/ICustomersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+@injectable()
+class CreateCustomerService {
+  constructor(private customersRepository: ICustomersRepository) {}
+
+  public async execute({ name, email }: IRequest): Promise<Customer> {
+    // TODO
+    const customerExist = await this.customersRepository.findByEmail(email);
+
+    if (customerExist) {
+      throw new AppError('Customer already exist');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
+  }
+}
+
+export default CreateCustomerService;
